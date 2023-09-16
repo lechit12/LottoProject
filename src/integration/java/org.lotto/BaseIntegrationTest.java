f7a856433835867d65a4372dda86194742daffbb
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2023-09-16 15:53:47 +0200

    added integration test

---
+++ src/integration/java/org.lotto/BaseIntegrationTest.java
@@ -0,0 +1,16 @@
+package org.lotto;
+
+import org.junit.jupiter.api.Test;
+import org.lotto.domain.LottoSpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LottoSpringBootApplication.class)
+public class BaseIntegrationTest {
+
+    @Test
+    public void f()
+    {
+
+    }
+
+}
